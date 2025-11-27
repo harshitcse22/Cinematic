b924683b60378f3eb65c802bad9b89808ce1b754
Harshit <hktiwari980@gmail.com>
2025-11-28 01:00:34 +0530

    Hero Section Completed

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'
 import marvelLogo from './marvelLogo.svg'
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
@@ -6,7 +5,6 @@ import screenImage from './screenImage.svg'
 import profile from './profile.png'
 
 export const assets = {
-    logo,
     marvelLogo,
     googlePlay,
     appStore,
